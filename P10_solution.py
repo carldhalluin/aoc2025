2bbdcb4ddffbfaac483cb6dfb5f6d618c904257d
Carl D'Halluin <carl.dhalluin@datadobi.com>
2025-12-23 20:03:05 +0100

    P10 part 2

--- P10_solution.py
+++ P10_solution.py
@@ -1,3 +1,6 @@
+import scipy
+import numpy as np
+
 from P10_input import PRODUCTION_INPUT
 
 TEST_INPUT = '''
@@ -6,34 +9,46 @@ TEST_INPUT = '''
 [.###.#] (0,1,2,3,4) (0,3,4) (0,1,2,4,5) (1,2) {10,11,11,5,10,5}
 '''
 
-def solve_part1(INPUT, mode):
-    problems = list()
+def solve(INPUT, mode):
+    problems1 = list()
+    problems2 = list()
     for row in INPUT.strip().splitlines():
         pieces = row.split(' ')
         assert pieces[0][0] == '['
         assert pieces[0][-1] == ']'
-        goal = 0
+        goal1 = 0
         for i in range(len(pieces[0])-2):
             pos = len(pieces[0])-i-2
             if pieces[0][pos] == '#':
-                goal |= 1 << i
-        goalsize = len(pieces[0])-2
-        buttons = list()
-        problem = [goal, buttons]
+                goal1 |= 1 << i
+        size = len(pieces[0])-2
+        buttons1 = list()
+        buttons2 = list()
+        goal2 = list()
+        problem1 = [goal1, buttons1]
         for piece in pieces[1:-1]:
             assert piece[0] == '('
             assert piece[-1] == ')'
-            value = 0
+            value1 = 0
+            value2 = [0] * size
             for num in piece[1:-1].split(','):
-                value |= 1 << (goalsize-1-int(num))
-            buttons.append(value)
-        problems.append(problem)
+                value1 |= 1 << (size-1-int(num))
+                value2[int(num)] = 1
+            buttons1.append(value1)
+            buttons2.append(tuple(value2))
+        assert pieces[-1][0] == '{'
+        assert pieces[-1][-1] == '}'
+        for value in pieces[-1][1:-1].split(','):
+            goal2.append(int(value))
+        problem2 = [np.ones(len(buttons2)), np.matrix(buttons2).transpose(), np.array(goal2)]
+        problems1.append(problem1)
+        problems2.append(problem2)
 
-    #print(f'Problems: {problems}')
+    #print('\nProblem1:', problems1)
+    #print('\nProblem2:', problems2)
 
-    total = 0
-    for goal, buttons in problems:
-        #print(f'\n\ngoal {goal} buttons {buttons}')
+    total1 = 0
+    for goal1, buttons in problems1:
         num_buttons = len(buttons)
         best_weight = 0
         for s in range(2**num_buttons):
@@ -43,17 +58,28 @@ def solve_part1(INPUT, mode):
                 if s & (1<<j):
                     value ^= buttons[j]
                     weight += 1
-                    #print(f'Pressed button {j} to get value {value}')
-            #print(f'value {value} weight {weight}')
             if weight >= best_weight > 0:
                 continue
-            if value == goal:
-                #print(f'Goal {goal} reached in {weight}: {bin(s)[2:]} * {[bin(button)[2:] for button in buttons]}')
+            if value == goal1:
                 if best_weight == 0 or weight < best_weight:
                     best_weight = weight
-        #print(f'best weight for goal {goal} = {best_weight}')
-        total += best_weight
-    print(f'{mode}: total={total}')
+        total1 += best_weight
+    print(f'\nPart1 - {mode}: total={total1}')
+    print('='*50)
 
-solve_part1(TEST_INPUT, 'test')
-solve_part1(PRODUCTION_INPUT, 'prod')
+    # Problem2 is linear programming
+    # * Minimize sum of x
+    # * A.x = b
+    total2 = 0
+    for c, A_eq, b_eq in problems2:
+        #print(c)
+        #print(A_eq)
+        #print(b_eq)
+        solution = scipy.optimize.linprog(c, A_eq=A_eq, b_eq=b_eq, integrality=True)
+        total2 += int(solution.fun)
+        #print(f'Solution: {int(solution.fun)}')
+    print(f'\nPart2 - {mode}: total={total2}')
+    print('='*50)
+
+solve(TEST_INPUT, 'test')
+solve(PRODUCTION_INPUT, 'prod')
